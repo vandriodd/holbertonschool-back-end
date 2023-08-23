15101df07a77e433326c438f1ae8740794e5ec4f
vandriodd <ludasaadi@gmail.com>
2023-08-22 17:59:49 -0700

    del backslash

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,19 +10,18 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    employee_id = argv[1]
+    id = argv[1]
 
     employee_info = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+        f'https://jsonplaceholder.typicode.com/users/{id}')
     employee_todo_list = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+        f'https://jsonplaceholder.typicode.com/users/{id}/todos')
 
     name = employee_info.json().get('username')
-    filename = f"{employee_id}.csv"
+    filename = f"{id}.csv"
     with open(filename, "w", encoding="utf-8") as file:
         for todo in employee_todo_list.json():
             completed_status = todo.get('completed')
             task_title = todo.get('title')
             file.write(
-                f'"{employee_id}","{name}",\
-                    "{completed_status}","{task_title}"\n')
+                f'"{id}","{name}","{completed_status}","{task_title}"\n')
